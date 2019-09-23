ee3b069a76bb7e9da88a0da70facab5535057f9c
Skostiuk4 <Skostiuk4@gmail.com>
2019-09-23 17:47:57 +0300

    first commit

---
+++ index1.php
@@ -0,0 +1,23 @@
+<?php
+//function multi
+function plus($a, $b) {
+	$add = $a + $b;
+	$subt = $a - $b;
+	$multiplication = $a * $b;
+	$division = $a / $b;
+
+	return array($add, $subt, $multiplication, $division);
+	
+}
+
+$result_array = plus(10, 3.7);
+echo "add: " . $result_array[0] . "<br />";
+echo "subt: " . $result_array[1] . "<br />";
+echo "multiplication: " . $result_array[2] . "<br />";
+echo "division: " . $result_array[3] . "<br />";
+
+
+?>
+
+
+
