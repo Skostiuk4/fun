6214a49db94cbec4512f2ecdc23b22aae5d24f78
Skostiuk4 <Skostiuk4@gmail.com>
2019-09-25 14:40:53 +0300

    second commit

--- index1.php
+++ index1.php
@@ -1,20 +1,20 @@
 <?php
 	function mathOperation($arg1, $arg2, $operation) {
-	  switch($operation){
-	  case "+":
-	    return $arg1+$arg2;
-	    break;
-	  case "-":
-	    return $arg1-$arg2;
-	    break;
-	  case "*":
-	    return $arg1*$arg2;
-	    break;
-	  case "/":
-	    return $arg1/$arg2;
-	    break;
-	  default:
-	    return "Некорректно введенная опция!";
+			switch($operation){
+			case "+":
+					return $arg1+$arg2;
+					break;
+			case "-":
+					return $arg1-$arg2;
+					break;
+			case "*":
+					return $arg1*$arg2;
+					break;
+			case "/":
+					return $arg1/$arg2;
+					break;
+			default:
+					return "Некорректно введенная опция!";
 	}
 	}
 	echo mathOperation(30, 3, "/");
