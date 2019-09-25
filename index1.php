c48cf9d2d917de26c8b07aed4e1b4453defc5f92
Skostiuk4 <Skostiuk4@gmail.com>
2019-09-25 15:03:17 +0300

    Fixed code styles

--- index1.php
+++ index1.php
@@ -1,24 +1,25 @@
 <?php
 
-  function mathOperation($arg1, $arg2, $operation) {
-    switch ($operation) {
+function mathOperation($arg1, $arg2, $operation) {
+  switch ($operation) {
     case "+":
-    		return $arg1 + $arg2;
-    		break;
+      return $arg1 + $arg2;
+      break;
     case "-":
-    		return $arg1 - $arg2;
-    		break;
+      return $arg1 - $arg2;
+      break;
     case "*":
-    		return $arg1 * $arg2;
-    		break;
+      return $arg1 * $arg2;
+      break;
     case "/":
-    		return $arg1 / $arg2;
-    		break;
+      return $arg1 / $arg2;
+      break;
     default:
-    		return 'Некорректно введенная опция!';
-    }
+      return 'Некорректно введенная опция!';
+      break;
   }
+}
 
-  echo mathOperation(15, 3, "/");
+echo mathOperation(15, 3, "/");
 
 ?>
