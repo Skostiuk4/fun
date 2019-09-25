b9236317267c01cf84db8cba8a1b6c52c4cae9cf
Skostiuk4 <Skostiuk4@gmail.com>
2019-09-25 10:35:43 +0300

    second commit

--- index1.php
+++ index1.php
@@ -2,11 +2,11 @@
 function mathOperation($arg1, $arg2, $operation) {
 switch($operation) {
   case "+":
-	return $arg1+$arg2;
-	break;
+    return $arg1+$arg2;
+    break;
   case "-":
-	return $arg1-$arg2;
-	break;
+    return $arg1-$arg2;
+    break;
   case "*":
 	return $arg1*$arg2;
 	break;
