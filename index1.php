0ffa23fbbce2e2cce89e423be6d7978f5126963c
Skostiuk4 <Skostiuk4@gmail.com>
2019-09-26 11:52:03 +0300

    second commit

--- index1.php
+++ index1.php
@@ -1,31 +1,31 @@
 <?php
 
 function mathOperation($arg1, $arg2, $operation) {
-  switch ($operation) {
-    case "+":
-      return $arg1 + $arg2;
-      break;
-    case "-":
-      return $arg1 - $arg2;
-      break;
-    case "*":
-      return $arg1 * $arg2;
-      break;
-    case "/":
-      return $arg1 / $arg2;
-      break;
-    case 'rand'();
-      return rand(10, 100);    
-      break;
-      return 'Некорректно введенная опция!';
-    default:
-  }
+ switch ($operation) {
+   case '+':
+     return $arg1 + $arg2;
+     break;
+   case '-':
+     return $arg1 - $arg2;
+     break;
+   case '*':
+     return $arg1 * $arg2;
+     break;
+   case '/':
+     return $arg1 / $arg2;
+     break;
+   case 'rand':
+     return rand($arg1, $arg2);
+     break;
+   default:
+     return 'Некорректно введенная опция!';
+   break;
+}
 }
 
-echo mathOperation(15, 3, "/");
-echo "<pre>";
-print_r(rand(10, 100));
-echo "<pre>";
-    
+echo mathOperation(3, 30, 'rand');
 
 ?>
+    
+
+
